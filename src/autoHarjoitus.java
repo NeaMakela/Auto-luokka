ba30511923e9d4703015738981e55777094f7d9d
NeaMakela <makelanea@hotmail.com>
2022-12-15 00:17:26 +0200

    Konstruktorit
    Luotu oletusmuodostin ja parametrinen muodostin

--- src/autoHarjoitus.java
+++ src/autoHarjoitus.java
@@ -3,12 +3,15 @@ public class autoHarjoitus {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Auto auto = new Auto();
-		auto.merkki="Saab";
-		auto.malli="9-3";
-		auto.bensanMaara=10;
-		auto.naytaTiedot();
-		auto.tankkaa(70);
+//		Auto auto = new Auto();
+//		auto.merkki="Saab";
+//		auto.malli="9-3";
+//		auto.bensanMaara=10;
+//		auto.naytaTiedot();
+//		auto.tankkaa(70);
+		
+		Auto auto2 = new Auto("Saab", "9-3", 20);
+		auto2.naytaTiedot();
 	}
 
 }//EndOfMain
@@ -19,6 +22,20 @@ class Auto
 	public String malli;
 	public int bensanMaara;
 
+	//Oletusmuodostin
+	public Auto()
+	{
+		merkki = "Saab";
+		malli = "9-5";
+		bensanMaara = 40;
+	}
+	
+	public Auto(String merkki, String malli, int bensanMaara)
+	{
+		this.merkki = merkki;
+		this.malli = malli;
+		this.bensanMaara = bensanMaara;
+	}
 // Metodit
 public void jarruta()
 {
